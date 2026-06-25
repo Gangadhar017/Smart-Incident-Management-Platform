a24b2cff3b8487f2be0d1f03b1287f697e05d959
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:19 +0530

    Complete implementation of DepartmentDto.java

--- auth-service/src/main/java/com/enterprise/incident/auth/dto/DepartmentDto.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/dto/DepartmentDto.java
@@ -3,3 +3,9 @@ package com.enterprise.incident.auth.dto;
 import lombok.Data;
 
 @Data
+public class DepartmentDto {
+    private Long id;
+    private String name;
+    private String code;
+    private Long managerId;
+}
