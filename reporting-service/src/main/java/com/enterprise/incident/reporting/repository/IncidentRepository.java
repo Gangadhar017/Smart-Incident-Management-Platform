affbc6bc8de57970758fa77c6ea54bbc12421cb8
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:26 +0530

    Complete implementation of IncidentRepository.java

--- reporting-service/src/main/java/com/enterprise/incident/reporting/repository/IncidentRepository.java
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/repository/IncidentRepository.java
@@ -4,3 +4,10 @@ import com.enterprise.incident.reporting.entity.Incident;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface IncidentRepository extends JpaRepository<Incident, Long> {
+    List<Incident> findByStatus(String status);
+    List<Incident> findByPriority(String priority);
+}
