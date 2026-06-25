01ec90459af6afcaaac2200c9563137f3c922a34
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:10 +0530

    Complete implementation of SlaRuleRepository.java

--- incident-service/src/main/java/com/enterprise/incident/incident/repository/SlaRuleRepository.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/SlaRuleRepository.java
@@ -4,3 +4,10 @@ import com.enterprise.incident.incident.entity.Priority;
 import com.enterprise.incident.incident.entity.SlaRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SlaRuleRepository extends JpaRepository<SlaRule, Long> {
+    Optional<SlaRule> findByPriority(Priority priority);
+}
