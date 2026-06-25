d78b198fe1861df43be15b8dcd6a1b328c166dee
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:23 +0530

    Implement core fields and structure for ReportingApplication.java

--- reporting-service/src/main/java/com/enterprise/incident/reporting/ReportingApplication.java
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/ReportingApplication.java
@@ -3,5 +3,3 @@ package com.enterprise.incident.reporting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-// Declared class framework
