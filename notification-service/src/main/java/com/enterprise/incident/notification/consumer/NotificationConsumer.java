ffea5cd4c70fb39fa5a1bccb6057235f7a289f35
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:21 +0530

    Initialize framework structure for NotificationConsumer.java

---
+++ notification-service/src/main/java/com/enterprise/incident/notification/consumer/NotificationConsumer.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.notification.consumer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+
+// Declared class framework
