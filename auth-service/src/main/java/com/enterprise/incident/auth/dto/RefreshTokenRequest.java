0ec7845fe64751900825a2a82032cba1a68dee3f
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:21 +0530

    Complete implementation of RefreshTokenRequest.java

--- auth-service/src/main/java/com/enterprise/incident/auth/dto/RefreshTokenRequest.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/dto/RefreshTokenRequest.java
@@ -3,3 +3,8 @@ package com.enterprise.incident.auth.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+@Data
+public class RefreshTokenRequest {
+    @NotBlank(message = "Refresh token is required")
+    private String refreshToken;
+}
