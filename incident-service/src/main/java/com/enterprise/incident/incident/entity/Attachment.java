056781597dbb9755394a6a269591fcde99a5863b
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:03 +0530

    Implement core fields and structure for Attachment.java

--- incident-service/src/main/java/com/enterprise/incident/incident/entity/Attachment.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/entity/Attachment.java
@@ -3,5 +3,21 @@ package com.enterprise.incident.incident.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-// Declared class framework
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attachments")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
