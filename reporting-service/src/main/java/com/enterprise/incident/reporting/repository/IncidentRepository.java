58e4d97ae6dcc27f81bce89778a32e1277c605a1
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:26 +0530

    Implement core fields and structure for IncidentRepository.java

--- reporting-service/src/main/java/com/enterprise/incident/reporting/repository/IncidentRepository.java
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/repository/IncidentRepository.java
@@ -4,4 +4,3 @@ import com.enterprise.incident.reporting.entity.Incident;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Declared class framework
