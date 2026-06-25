963516852e78c896739e0d11090fb728ee8108e5
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:21 +0530

    Complete implementation of NotificationApplication.java

--- notification-service/src/main/java/com/enterprise/incident/notification/NotificationApplication.java
+++ notification-service/src/main/java/com/enterprise/incident/notification/NotificationApplication.java
@@ -3,3 +3,9 @@ package com.enterprise.incident.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+}
