d967587b6678b09b65006b0bfd3151dd12d4001d
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:27 +0530

    Implement core fields and structure for ReportingService.java

--- reporting-service/src/main/java/com/enterprise/incident/reporting/service/ReportingService.java
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/service/ReportingService.java
@@ -2,6 +2,3 @@ package com.enterprise.incident.reporting.service;
 
 import com.enterprise.incident.reporting.dto.DashboardKpiDto;
 
-public interface ReportingService {
-
-// Declared class framework
