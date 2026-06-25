126aaaf141b6852f3e2d5a6d5807c110c8e289e5
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:43 +0530

    Initialize framework structure for app.component.ts

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,5 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api.service';
+
