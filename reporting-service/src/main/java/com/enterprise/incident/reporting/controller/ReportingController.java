6e9e74c6805fe8a49cf3d8debf4411c7099e34fc
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:24 +0530

    Initialize framework structure for ReportingController.java

---
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/controller/ReportingController.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.reporting.controller;
+
+import com.enterprise.incident.reporting.dto.DashboardKpiDto;
+import com.enterprise.incident.reporting.service.ReportingService;
+import lombok.RequiredArgsConstructor;
+
+// Declared class framework
