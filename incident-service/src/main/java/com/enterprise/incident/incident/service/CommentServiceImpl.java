62e3a0774e290d2bdff5635b37a0a62b5abe16eb
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:14 +0530

    Initialize framework structure for CommentServiceImpl.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/service/CommentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.service;
+
+import com.enterprise.incident.incident.client.AuthServiceClient;
+import com.enterprise.incident.incident.dto.CommentDto;
+import com.enterprise.incident.incident.dto.UserDto;
+
+// Declared class framework
