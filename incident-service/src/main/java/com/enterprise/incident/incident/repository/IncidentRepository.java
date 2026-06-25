9e6557b893666875719b3a96fa2bb292d9d974c7
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:09 +0530

    Initialize framework structure for IncidentRepository.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/IncidentRepository.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.repository;
+
+import com.enterprise.incident.incident.entity.Incident;
+import com.enterprise.incident.incident.entity.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Declared class framework
