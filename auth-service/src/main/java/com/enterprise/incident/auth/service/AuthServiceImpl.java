990bd6eda16a581166686f369bcb823fa1b62f3f
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:34 +0530

    Initialize framework structure for AuthServiceImpl.java

---
+++ auth-service/src/main/java/com/enterprise/incident/auth/service/AuthServiceImpl.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.service;
+
+import com.enterprise.incident.auth.dto.*;
+import com.enterprise.incident.auth.entity.Department;
+import com.enterprise.incident.auth.entity.Role;
+
+// Declared class framework
