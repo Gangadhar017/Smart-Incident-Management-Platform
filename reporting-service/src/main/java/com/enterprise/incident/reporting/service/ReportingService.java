376e8e25f91d6817e52782da82fc3bfb9cb5a140
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:26 +0530

    Initialize framework structure for ReportingService.java

---
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/service/ReportingService.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.reporting.service;
+
+import com.enterprise.incident.reporting.dto.DashboardKpiDto;
+
+public interface ReportingService {
+
+// Declared class framework
