cce2a2cee6e5a3403498c63d6f446696b2ba2d36
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:48 +0530

    Initialize framework structure for api.service.ts

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, BehaviorSubject, tap } from 'rxjs';
+
+@Injectable({
