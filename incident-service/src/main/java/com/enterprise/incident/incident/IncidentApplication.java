468ab6b9dff4fbb5d9db7aada276573ef1951e2e
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:53 +0530

    Complete implementation of IncidentApplication.java

--- incident-service/src/main/java/com/enterprise/incident/incident/IncidentApplication.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/IncidentApplication.java
@@ -6,3 +6,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.enterprise.incident.incident.client")
+@EnableJpaAuditing
+@EnableScheduling
+public class IncidentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentApplication.class, args);
+    }
+}
