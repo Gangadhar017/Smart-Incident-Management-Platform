8c7ae529b65d9e627157f69059e1e52d8c3a5699
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:09 +0530

    Implement core fields and structure for AuthApplication.java

--- auth-service/src/main/java/com/enterprise/incident/auth/AuthApplication.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/AuthApplication.java
@@ -4,4 +4,3 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// Declared class framework
