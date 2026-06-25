ed5ebbebddb48b4907eb71ba1b82b2d07e67ed12
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:09 +0530

    Initialize framework structure for CommentRepository.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.repository;
+
+import com.enterprise.incident.incident.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Declared class framework
