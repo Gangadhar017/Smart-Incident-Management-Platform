9eb37ad90e31942a926d56f290223d4348c27830
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:15 +0530

    Initialize framework structure for IncidentService.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/service/IncidentService.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.service;
+
+import com.enterprise.incident.incident.dto.CreateIncidentRequest;
+import com.enterprise.incident.incident.dto.IncidentDto;
+import com.enterprise.incident.incident.dto.UpdateIncidentRequest;
+
+// Declared class framework
