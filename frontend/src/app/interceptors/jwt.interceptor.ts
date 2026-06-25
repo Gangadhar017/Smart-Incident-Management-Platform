d12234a9a9a2d47b0e3d9488f4a21fbd95968b20
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:48 +0530

    Implement core fields and structure for jwt.interceptor.ts

--- frontend/src/app/interceptors/jwt.interceptor.ts
+++ frontend/src/app/interceptors/jwt.interceptor.ts
@@ -3,3 +3,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access_token');
   
+  // Don't append if it's logging in or refreshing
+  if (token && !req.url.includes('/api/v1/auth/')) {
+    const cloned = req.clone({
