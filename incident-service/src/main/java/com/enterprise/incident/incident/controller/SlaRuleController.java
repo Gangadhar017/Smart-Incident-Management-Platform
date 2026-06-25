c0090b7262f6f3b9d631faad369780096e912394
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:56 +0530

    Initialize framework structure for SlaRuleController.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/controller/SlaRuleController.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.controller;
+
+import com.enterprise.incident.incident.entity.SlaRule;
+import com.enterprise.incident.incident.repository.SlaRuleRepository;
+import lombok.RequiredArgsConstructor;
+
+// Declared class framework
