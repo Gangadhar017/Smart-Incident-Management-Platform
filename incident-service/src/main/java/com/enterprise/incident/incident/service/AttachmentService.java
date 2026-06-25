f18f91119f51d4986d539bbe7e9792d142d48ce2
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:11 +0530

    Complete implementation of AttachmentService.java

--- incident-service/src/main/java/com/enterprise/incident/incident/service/AttachmentService.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/service/AttachmentService.java
@@ -3,3 +3,9 @@ package com.enterprise.incident.incident.service;
 import com.enterprise.incident.incident.dto.AttachmentDto;
 
 import java.util.List;
+
+public interface AttachmentService {
+    AttachmentDto uploadFile(Long incidentId, String filename, String contentType, byte[] data, String token);
+    byte[] downloadFile(Long attachmentId, String token);
+    List<AttachmentDto> getAttachmentsForIncident(Long incidentId, String token);
+}
