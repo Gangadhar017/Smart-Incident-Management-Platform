c4346bf70a7d6dd97f204a69cbe9c7986165d86a
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:51 +0530

    Complete implementation of GatewayApplication.java

--- gateway-service/src/main/java/com/enterprise/incident/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/enterprise/incident/gateway/GatewayApplication.java
@@ -3,3 +3,9 @@ package com.enterprise.incident.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
