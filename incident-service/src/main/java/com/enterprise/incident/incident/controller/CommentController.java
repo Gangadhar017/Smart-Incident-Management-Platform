147e7a21dff6968bf79d581b9ed22b8fb4e0c153
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:55 +0530

    Initialize framework structure for CommentController.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/controller/CommentController.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.controller;
+
+import com.enterprise.incident.incident.dto.CommentDto;
+import com.enterprise.incident.incident.service.CommentService;
+import lombok.RequiredArgsConstructor;
+
+// Declared class framework
