6f382fcc5db0ec23790605550d21274caa80d1f1
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:44 +0530

    Implement core fields and structure for app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,3 +3,6 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { routes } from './app.routes';
+import { jwtInterceptor } from './interceptors/jwt.interceptor';
+
