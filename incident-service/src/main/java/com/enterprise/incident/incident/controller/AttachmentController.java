386e9427e7602914c52207adc65fa2029868af3d
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:54 +0530

    Initialize framework structure for AttachmentController.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/controller/AttachmentController.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.controller;
+
+import com.enterprise.incident.incident.dto.AttachmentDto;
+import com.enterprise.incident.incident.service.AttachmentService;
+import lombok.RequiredArgsConstructor;
+
+// Declared class framework
