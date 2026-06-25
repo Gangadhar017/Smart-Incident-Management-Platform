62136f5449898ab86847b285c2470ceeff334db6
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:35 +0530

    Initialize framework structure for AuthServiceTest.java

---
+++ auth-service/src/test/java/com/enterprise/incident/auth/service/AuthServiceTest.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.service;
+
+import com.enterprise.incident.auth.dto.CreateUserRequest;
+import com.enterprise.incident.auth.dto.LoginRequest;
+import com.enterprise.incident.auth.dto.LoginResponse;
+
+// Declared class framework
