8511f300f7906267a515f75517781adb31719f0b
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:30 +0530

    Complete implementation of TeamRepository.java

--- auth-service/src/main/java/com/enterprise/incident/auth/repository/TeamRepository.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/repository/TeamRepository.java
@@ -4,3 +4,9 @@ import com.enterprise.incident.auth.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findByDepartmentId(Long departmentId);
+}
