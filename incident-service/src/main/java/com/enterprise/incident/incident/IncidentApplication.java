ffc00a1fea22ac0307cd539edc26fb5e04378a04
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:53 +0530

    Implement core fields and structure for IncidentApplication.java

--- incident-service/src/main/java/com/enterprise/incident/incident/IncidentApplication.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/IncidentApplication.java
@@ -3,5 +3,6 @@ package com.enterprise.incident.incident;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-// Declared class framework
