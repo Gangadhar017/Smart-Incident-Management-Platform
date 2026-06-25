8b50dda6b218655e650fde098b85bd70959698be
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:23 +0530

    Complete implementation of TokenResponse.java

--- auth-service/src/main/java/com/enterprise/incident/auth/dto/TokenResponse.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/dto/TokenResponse.java
@@ -5,3 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
