07bcc807d355b1988fdbbbaf9c02922aa968e808
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:06 +0530

    Implement core fields and structure for SlaRule.java

--- incident-service/src/main/java/com/enterprise/incident/incident/entity/SlaRule.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/entity/SlaRule.java
@@ -3,5 +3,12 @@ package com.enterprise.incident.incident.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name = "sla_rules")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SlaRule {
 
-// Declared class framework
