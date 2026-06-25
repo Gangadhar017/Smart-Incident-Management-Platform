435404946a0893a710ea7b2a2c54da3f16e34276
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:10 +0530

    Complete implementation of IncidentRepository.java

--- incident-service/src/main/java/com/enterprise/incident/incident/repository/IncidentRepository.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/IncidentRepository.java
@@ -6,3 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface IncidentRepository extends JpaRepository<Incident, Long>, JpaSpecificationExecutor<Incident> {
+    Optional<Incident> findByIncidentNumber(String incidentNumber);
+    List<Incident> findBySlaBreachedFalseAndStatusNotIn(List<Status> statuses);
+}
