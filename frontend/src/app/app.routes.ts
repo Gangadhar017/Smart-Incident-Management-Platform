25f96e1009dd10873afaed8a2227558f58f68a7e
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:44 +0530

    Complete implementation of app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -14,3 +14,20 @@ export const routes: Routes = [
     loadComponent: () => import('./incidents/incident-list.component').then(m => m.IncidentListComponent)
   },
   {
+    path: 'incidents/:id',
+    loadComponent: () => import('./incidents/incident-detail.component').then(m => m.IncidentDetailComponent)
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent)
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard'
+  }
+];
