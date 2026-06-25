aaf571812375492c0382420868ec25f4c10ed872
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:08 +0530

    Complete implementation of AuditLogRepository.java

--- incident-service/src/main/java/com/enterprise/incident/incident/repository/AuditLogRepository.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/AuditLogRepository.java
@@ -4,3 +4,9 @@ import com.enterprise.incident.incident.entity.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByIncidentIdOrderByTimestampDesc(Long incidentId);
+}
