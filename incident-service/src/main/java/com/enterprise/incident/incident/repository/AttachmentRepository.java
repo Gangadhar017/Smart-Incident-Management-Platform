4d9c34f013202f442cc08e0d32ad182d6d5cf2f3
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:08 +0530

    Complete implementation of AttachmentRepository.java

--- incident-service/src/main/java/com/enterprise/incident/incident/repository/AttachmentRepository.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/AttachmentRepository.java
@@ -4,3 +4,9 @@ import com.enterprise.incident.incident.entity.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+    List<Attachment> findByIncidentId(Long incidentId);
+}
