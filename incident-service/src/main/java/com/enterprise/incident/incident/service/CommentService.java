f85da57766383361df82997d3bf13142b9cacb7b
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:13 +0530

    Complete implementation of CommentService.java

--- incident-service/src/main/java/com/enterprise/incident/incident/service/CommentService.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/service/CommentService.java
@@ -3,3 +3,8 @@ package com.enterprise.incident.incident.service;
 import com.enterprise.incident.incident.dto.CommentDto;
 
 import java.util.List;
+
+public interface CommentService {
+    CommentDto addComment(Long incidentId, CommentDto commentDto, String token);
+    List<CommentDto> getCommentsForIncident(Long incidentId, String token);
+}
