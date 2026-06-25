8911fcd39b16c11fbaed294f7528681739b68335
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:45 +0530

    Initialize framework structure for admin.component.ts

---
+++ frontend/src/app/admin/admin.component.ts
@@ -0,0 +1,5 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+
