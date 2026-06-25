719d017356df7b9d6ad2965074d9b74982b1c22a
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:00 +0530

    Initialize framework structure for IncidentDto.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/dto/IncidentDto.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.dto;
+
+import com.enterprise.incident.incident.entity.Priority;
+import com.enterprise.incident.incident.entity.Status;
+import lombok.AllArgsConstructor;
+
+// Declared class framework
