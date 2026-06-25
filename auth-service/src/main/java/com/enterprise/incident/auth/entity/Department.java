5bf13be301fa2933cebd9b578d8d93914bc491d0
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:24 +0530

    Initialize framework structure for Department.java

---
+++ auth-service/src/main/java/com/enterprise/incident/auth/entity/Department.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+// Declared class framework
