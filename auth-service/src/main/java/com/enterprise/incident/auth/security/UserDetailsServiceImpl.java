b98241ddce7a42da6194bf50e348f95b494f307e
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:32 +0530

    Initialize framework structure for UserDetailsServiceImpl.java

---
+++ auth-service/src/main/java/com/enterprise/incident/auth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.security;
+
+import com.enterprise.incident.auth.entity.User;
+import com.enterprise.incident.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+// Declared class framework
