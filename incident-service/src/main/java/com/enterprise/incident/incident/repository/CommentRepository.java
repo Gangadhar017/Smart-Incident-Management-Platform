de558f6f76d844bb42532010efa96e2721a9ae39
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:09 +0530

    Implement core fields and structure for CommentRepository.java

--- incident-service/src/main/java/com/enterprise/incident/incident/repository/CommentRepository.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/repository/CommentRepository.java
@@ -4,4 +4,3 @@ import com.enterprise.incident.incident.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Declared class framework
