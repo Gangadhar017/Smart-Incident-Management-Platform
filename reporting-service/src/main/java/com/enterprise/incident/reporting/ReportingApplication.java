ce4c5c33497be61163b67db4a4863435180ea3f4
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:23 +0530

    Initialize framework structure for ReportingApplication.java

---
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/ReportingApplication.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// Declared class framework
