91c72069e5be10143e6e545208e9008c3f1d2e24
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:53 +0530

    Implement core fields and structure for AuthServiceClient.java

--- incident-service/src/main/java/com/enterprise/incident/incident/client/AuthServiceClient.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/client/AuthServiceClient.java
@@ -3,5 +3,6 @@ package com.enterprise.incident.incident.client;
 import com.enterprise.incident.incident.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-// Declared class framework
