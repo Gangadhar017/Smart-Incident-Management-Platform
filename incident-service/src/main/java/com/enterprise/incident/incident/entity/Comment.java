bcdc1dde2e36bf63b39159bd884030c11f451d6b
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:04 +0530

    Implement core fields and structure for Comment.java

--- incident-service/src/main/java/com/enterprise/incident/incident/entity/Comment.java
+++ incident-service/src/main/java/com/enterprise/incident/incident/entity/Comment.java
@@ -3,5 +3,18 @@ package com.enterprise.incident.incident.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-// Declared class framework
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
