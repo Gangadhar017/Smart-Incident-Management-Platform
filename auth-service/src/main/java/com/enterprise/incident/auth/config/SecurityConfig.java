945c146213ded433045d804bca2c4d04072ed75d
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:10 +0530

    Initialize framework structure for SecurityConfig.java

---
+++ auth-service/src/main/java/com/enterprise/incident/auth/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.config;
+
+import com.enterprise.incident.auth.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+
+// Declared class framework
