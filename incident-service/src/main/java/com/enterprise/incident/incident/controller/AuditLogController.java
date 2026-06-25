8e3cc4f8a02137a0640fe029acde420f1b1392ad
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:54 +0530

    Initialize framework structure for AuditLogController.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/controller/AuditLogController.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.controller;
+
+import com.enterprise.incident.incident.dto.AuditLogDto;
+import com.enterprise.incident.incident.entity.AuditLog;
+import com.enterprise.incident.incident.repository.AuditLogRepository;
+
+// Declared class framework
