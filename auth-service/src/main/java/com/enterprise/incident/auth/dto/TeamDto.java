bd830b86031f7f4b93a121ca3addbe6c6b353453
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:22 +0530

    Complete implementation of TeamDto.java

--- auth-service/src/main/java/com/enterprise/incident/auth/dto/TeamDto.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/dto/TeamDto.java
@@ -3,3 +3,9 @@ package com.enterprise.incident.auth.dto;
 import lombok.Data;
 
 @Data
+public class TeamDto {
+    private Long id;
+    private String name;
+    private Long departmentId;
+    private Long leadId;
+}
