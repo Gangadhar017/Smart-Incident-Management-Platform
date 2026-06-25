219ae56ad9f34b072c5a2dd99494de5bdcb2a912
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:20 +0530

    Complete implementation of LoginRequest.java

--- auth-service/src/main/java/com/enterprise/incident/auth/dto/LoginRequest.java
+++ auth-service/src/main/java/com/enterprise/incident/auth/dto/LoginRequest.java
@@ -4,3 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+public class LoginRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
