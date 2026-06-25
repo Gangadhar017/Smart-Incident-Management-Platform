7b5fba30073efd7e9b81ad018fb7324d457ecd38
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:17 +0530

    Initialize framework structure for SlaEscalationScheduler.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/service/SlaEscalationScheduler.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+// Declared class framework
