ee08c012b86e59da69335aae81071552b9e614f6
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:53 +0530

    Initialize framework structure for AuthServiceClient.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/client/AuthServiceClient.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.client;
+
+import com.enterprise.incident.incident.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Declared class framework
