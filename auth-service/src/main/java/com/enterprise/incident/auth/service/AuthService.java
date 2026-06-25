77a55155488b9f9704887629a1d726bcdc065c67
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:33 +0530

    Initialize framework structure for AuthService.java

---
+++ auth-service/src/main/java/com/enterprise/incident/auth/service/AuthService.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.service;
+
+import com.enterprise.incident.auth.dto.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+// Declared class framework
