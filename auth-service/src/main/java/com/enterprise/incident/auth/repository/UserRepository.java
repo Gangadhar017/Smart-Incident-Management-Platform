0d2d793895b861e41c7d11730b19291f1adce13e
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:30 +0530

    Initialize framework structure for UserRepository.java

---
+++ auth-service/src/main/java/com/enterprise/incident/auth/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.auth.repository;
+
+import com.enterprise.incident.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+// Declared class framework
