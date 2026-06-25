d96a4b6d0f9c7393d06afda050180e3a69c591c3
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:05 +0530

    Initialize framework structure for Incident.java

---
+++ incident-service/src/main/java/com/enterprise/incident/incident/entity/Incident.java
@@ -0,0 +1,7 @@
+package com.enterprise.incident.incident.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+// Declared class framework
