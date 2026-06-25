38e8c039f0ee79f4fd064bd7cb2b025994b8ffb0
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:20 +0530

    Implement core fields and structure for NotificationApplication.java

--- notification-service/src/main/java/com/enterprise/incident/notification/NotificationApplication.java
+++ notification-service/src/main/java/com/enterprise/incident/notification/NotificationApplication.java
@@ -3,5 +3,3 @@ package com.enterprise.incident.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-// Declared class framework
