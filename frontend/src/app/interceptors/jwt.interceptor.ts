ea093941e62cc6c606d4d0dab95f03f844b39e29
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:04:48 +0530

    Initialize framework structure for jwt.interceptor.ts

---
+++ frontend/src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,5 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('access_token');
+  
