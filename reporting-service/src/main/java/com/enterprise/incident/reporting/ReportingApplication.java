9dcf3922c04ba7c559a588cdd219cdff14f499c0
Gangadhar017 <gangadhar017@users.noreply.github.com>
2026-06-25 11:05:24 +0530

    Complete implementation of ReportingApplication.java

--- reporting-service/src/main/java/com/enterprise/incident/reporting/ReportingApplication.java
+++ reporting-service/src/main/java/com/enterprise/incident/reporting/ReportingApplication.java
@@ -3,3 +3,9 @@ package com.enterprise.incident.reporting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class ReportingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingApplication.class, args);
+    }
+}
